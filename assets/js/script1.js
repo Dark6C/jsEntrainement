c11431a56445b85b6a8b02cf247ce0bafe2058a4
Matthieu <matthieu.ciceron@gmail.com>
2023-06-30 10:31:39 +0200

    début exo 3

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -9,10 +9,16 @@ let passwordEl = document.querySelector("#password");
 let passwordCheckEl = document.querySelector("#passwordCheck");
 let passwordHelp = document.querySelector("#passwordHelp");
 
+let passHelp = document.querySelector("#passHelp");
+
 //Variables et constantes
 
 const regexUsername = /^[a-zA-Z0-9]+$/;
 const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const regexWeak = /^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$/;
+const regexAverage = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+const regexStrong =
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
 //Fonctions
 
@@ -71,9 +77,40 @@ let checkPasswords = () => {
   }
 };
 
+let passwordStrength = () => {
+  passHelp.classList.add("d-none");
+  passHelp.classList.remove("text-success", "text-warning", "text-danger");
+  if (passwordEl.value.length > 0 && passwordEl.value.length < 8) {
+    passHelp.classList.remove("d-none");
+    passHelp.textContent = "Le mot de passe est très faible";
+    passHelp.classList.add("text-danger");
+    console.log("weak1");
+  } else if (passwordEl.value.length >= 8) {
+    if (regexStrong.test(passwordEl.value)) {
+      passHelp.classList.remove("d-none");
+      passHelp.classList.add("text-success");
+      passHelp.textContent = "Le mot de passe est fort";
+      console.log("fort");
+    } else if (regexAverage.test(passwordEl.value)) {
+      passHelp.classList.remove("d-none");
+      passHelp.textContent = "Le mot de passe est moyen";
+      passHelp.classList.add("text-warning");
+      console.log("medium");
+    } else if (regexWeak.test(passwordEl.value)) {
+      passHelp.classList.remove("d-none");
+      passHelp.textContent = "Le mot de passe est faible";
+      console.log("weak2");
+    } else {
+      passHelp.classList.remove("d-none");
+      passHelp.textContent = "Le mot de passe est très faible";
+      console.log("weak1");
+    }
+  }
+};
+
 //écouteurs
 usernameEl.addEventListener("input", checkUsername);
 emailEl.addEventListener("input", checkEmail);
 
-passwordEl.addEventListener("input", checkPasswords);
+passwordEl.addEventListener("input", passwordStrength);
 passwordCheckEl.addEventListener("input", checkPasswords);
