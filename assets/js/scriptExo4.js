43b9037696caea1309e4507936cc1d3ebae2ef60
Matthieu <matthieu.ciceron@gmail.com>
2023-06-30 16:15:17 +0200

    clonage du form au click du btn 'Ajouter une pers'

---
+++ assets/js/scriptExo4.js
@@ -0,0 +1,16 @@
+
+//Sélecteurs
+
+let newFormUserButton = document.getElementById("newFormUser");
+let mainForm = document.getElementById("mainForm");
+
+//Fonctions
+
+let clonage = () => {
+    let clonedForm = mainForm.cloneNode(true); 
+    mainForm.parentNode.appendChild(clonedForm);
+}
+
+//Ecouteurs
+
+newFormUserButton.addEventListener("click",clonage)
