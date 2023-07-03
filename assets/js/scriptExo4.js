28f4dd8751393a9882607c10a2ea2e3276434c0a
Matthieu <matthieu.ciceron@gmail.com>
2023-07-03 09:57:03 +0200

    changes exo 4

--- assets/js/scriptExo4.js
+++ assets/js/scriptExo4.js
@@ -1,16 +1,18 @@
-
 //Sélecteurs
 
 let newFormUserButton = document.getElementById("newFormUser");
 let mainForm = document.getElementById("mainForm");
 
+
 //Fonctions
 
 let clonage = () => {
-    let clonedForm = mainForm.cloneNode(true); 
-    mainForm.parentNode.appendChild(clonedForm);
-}
+  let clonedForm = mainForm.cloneNode(true);
+  mainForm.parentNode.appendChild(clonedForm);
+};
+
+
 
 //Ecouteurs
 
-newFormUserButton.addEventListener("click",clonage)
+newFormUserButton.addEventListener("click", clonage);
